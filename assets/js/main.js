9828c1eab6624f31587d074c6bb3f788469f5438
Salano24 <salernomarco48@email.com>
2022-10-12 17:02:22 +0200

    carosello funzionante senza infinito

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,71 @@
-const immaginiCarosello = [
-    "./img/01.webp",
-    "./img/02.webp",
-    "./img/03.webp",
-    "./img/04.webp",
-    "./img/05.webp"
-]
+/* Carosello */
+const slides = [
+    './img/01.webp',
+    './img/02.webp',
+    './img/03.webp',
+    './img/04.webp',
+    './img/05.webp',
 
-/* selezione l'elemento in cui andranno le immagini */
-const slidesEl = document.querySelector('.slides')
-let immagineAttiva= 0;
-/* scrivo il loop per attraversare l'array */
-for (let i = 0; i < immaginiCarosello.length; i++) {
-    
-    const immaginiUrl = immaginiCarosello[i];
-    const immaginiAttributo = `<img class="${i === immagineAttiva ? 'active' : ''}" src="${immaginiUrl}"`
-    slidesEl.insertAdjacentHTML('beforeend', immaginiAttributo);
-}
-
-
-/* creo le variabili per i pulsanti */
-const avanti = document.querySelector('.avanti')
-const indietro = document.querySelector('.indietro')
-
-/* verifico il funzionamento dei pulsanti in console */
-avanti.addEventListener('click', function(){    
     
     
-    console.log('avanti');
-    /* seleziono tutte le immagini per trovare quella attuale */
-    const immagini = document.querySelector('.slides > img');
-    const immagineAttuale = immagini[immagineAttiva]
-    /* tolgo la classe active */
-    immagineAttuale.remove('active')
-    /* incremento l'immagine attiva */
-    immagineAttiva++
-   /* seleziono limmagine successiva */
-   const immagineSuccessiva = immagini[immagineAttiva]
-   immagineSuccessiva.add('active')
-
-})
-indietro.addEventListener('click', function(){
-    console.log('indietro');
-})
+  ]
+  
+  
+  // Seleziono dom element where place the images
+  const slidesEl = document.querySelector('.slides')
+  
+  // Creo variabile per monitorare la slide attiva
+  let activeImage = 0;
+  
+  // loop inside the array
+  for (let i = 0; i < slides.length; i++) {
+    const slideUrl = slides[i];
+    const slideMarkup = `<img class="${i === activeImage ? 'active' : ''}" src="${slideUrl}" alt="">`
+    slidesEl.insertAdjacentHTML('beforeend', slideMarkup)
+  
+  }
+  
+  
+  // seleziono i pulsanti prev e next
+  const nextButtonEl = document.querySelector('.next');
+  const prevButtonEl = document.querySelector('.prev');
+  
+  
+  // Ascolto per il click sul pulsante next
+  nextButtonEl.addEventListener('click', function () {
+    console.log('next');
+    /* const currentSlide = document.querySelector('.slides > img.active')
+    console.log(currentSlide); */
+  
+    //Seleziono tutte le immagini nel markup con lo scopo di trovare qualla attiva
+    const slides = document.querySelectorAll('.slides > img'); // getElementsByClassName
+    const currentSlide = slides[activeImage]; // slides[0]
+    // tolgo classe active
+    currentSlide.classList.remove('active')
+    // incremento active image di uno
+    activeImage++
+    //seleziono la seconda immagine
+    const nextSlide = slides[activeImage]
+    console.log(nextSlide);
+    // gli applico la classe active
+    nextSlide.classList.add('active')
+  
+  })
+  // Ascolto per il click sul pulsante prev
+  prevButtonEl.addEventListener('click', function () {
+    console.log('prev');
+  
+    //Seleziono tutte le immagini nel markup con lo scopo di trovare qualla attiva
+    const slides = document.querySelectorAll('.slides > img');
+    const currentSlide = slides[activeImage];
+    // tolgo classe active
+    currentSlide.classList.remove('active')
+    // incremento active image di uno
+    activeImage--
+    //seleziono la seconda immagine
+    const nextSlide = slides[activeImage]
+    console.log(nextSlide);
+    // gli applico la classe active
+    nextSlide.classList.add('active')
+  
+  })
