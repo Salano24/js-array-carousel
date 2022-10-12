045183f958d8d39c9da7da0ca86cbdbb17dd0582
Salano24 <salernomarco48@email.com>
2022-10-12 15:07:00 +0200

    pulsanti funzionanti

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,27 @@ const immaginiCarosello = [
     "./img/04.webp",
     "./img/05.webp"
 ]
+
+/* selezione l'elemento in cui andranno le immagini */
+const slidesEl = document.querySelector('.slides')
+
+/* scrivo il loop per attraversare l'array */
+for (let i = 0; i < immaginiCarosello.length; i++) {
+    
+    const immaginiUrl = immaginiCarosello[i];
+    const immaginiAttributo = `<img class="${i === 0 ? 'active' : ''}" src="${immaginiUrl}"`
+    slidesEl.insertAdjacentHTML('beforeend', immaginiAttributo);
+}
+
+
+/* creo le variabili per i pulsanti */
+const avanti = document.querySelector('.avanti')
+const indietro = document.querySelector('.indietro')
+
+/* verifico il funzionamento dei pulsanti in console */
+avanti.addEventListener('click', function(){
+    console.log('avanti');
+})
+indietro.addEventListener('click', function(){
+    console.log('indietro');
+})
